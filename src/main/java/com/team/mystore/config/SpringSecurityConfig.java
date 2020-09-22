46e1362dc9cfd47de7513440fbe57f55fb088330
tuangh <tuangh@tuanghs-MacBook-Pro.local>
2020-09-22 19:32:14 +0700

    create security

--- src/main/java/com/team/mystore/config/SpringSecurityConfig.java
+++ src/main/java/com/team/mystore/config/SpringSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.team.mystore.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
 
 }
