30f75d7b6aea5fdc0a0b90bad0fdebd7ed1de7fc
nannkau <48847934+nannkau@users.noreply.github.com>
2020-09-22 15:24:41 +0700

    creat product

--- src/main/java/com/team/mystore/repository/IGenericRepository.java
+++ src/main/java/com/team/mystore/repository/IGenericRepository.java
@@ -1,9 +1,11 @@
 package com.team.mystore.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.io.Serializable;
 import java.util.List;
 
-public interface IGenericRepository<T extends Serializable> {
+public interface IGenericRepository<T extends Serializable> extends JpaRepository<T, Long> {
     T findOne(final Long id);
 
     List<T> findAll();
--- src/main/java/com/team/mystore/repository/IProductRepository.java
+++ src/main/java/com/team/mystore/repository/IProductRepository.java
@@ -2,6 +2,9 @@ package com.team.mystore.repository;
 
 import com.team.mystore.entity.Product;
 
+import java.util.Optional;
 
-public interface IProductRepository extends IGenericRepository<Product> {
+
+public interface IProductRepository extends IGenericRepository<Product>  {
+    Optional<Product> findById(Long id);
 }
